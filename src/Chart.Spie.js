4f2682c21dee2725dc78df2965509d94e7adfefd
Angus Macdonald <angus.d.macdonald@gmail.com>
2015-02-19 19:32:03 -0500

    improve (tooltip): add custom tooltip

--- src/Chart.Spie.js
+++ src/Chart.Spie.js
@@ -47,7 +47,10 @@
 		animateScale : false,
 
 		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+
+		tooltipTemplate: "<%if (label){%><%=label%><%}%>"
+
 	};
 
 
