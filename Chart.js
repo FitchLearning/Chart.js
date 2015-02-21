e3d7d1838f745ae603a472eea5d7352c0448f40b
Angus Macdonald <angus.d.macdonald@gmail.com>
2015-02-21 17:51:35 -0500

    improve (legend): created working legend example
    The spie-enhanced.html file contains a working legend example,
    including where highlghting an item in the legend results in the
    corresponding chart element being shown.

--- Chart.js
+++ Chart.js
@@ -3509,9 +3509,9 @@
 		animateScale : false,
 
 		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><ul><% for (var j=0; j<segments[i].slices.length; j++){%><li><span style=\"background-color:<%=segments[i].slices[j].fillColor%>\"></span><%if(segments[i].slices[j].label){%><%=segments[i].slices[j].segmentLabel%><%}%></li><%}%></ul><%}%></ul>",
+		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%><ul><% for (var j=0; j<segments[i].slices.length; j++){%><li><span style=\"background-color:<%=segments[i].slices[j].fillColor%>\"></span><%if(segments[i].slices[j].label){%><%=segments[i].slices[j].segmentLabel%><%}%></li><%}%></ul><%}%></li></ul>",
 
-		tooltipTemplate: "<%if (label){%><%=label%><%}%><%if (segmentLabel && label){%>-<%}%><%if (segmentLabel){%><%=segmentLabel%><%}%>"
+		tooltipTemplate: "<%if (label){%><%=label%><%}%><%if (segmentLabel && label){%> - <%}%><%if (segmentLabel){%><%=segmentLabel%><%}%>"
 
 	};
 
